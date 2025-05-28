213d1a608bedc8d3c91fde3cd8277ecd61139ac9
Leticia <leticiacsmagalhaes14@gmail.com>
2025-05-28 18:22:43 -0300

    salvar

--- questao05.sh
+++ questao05.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+cd ~
 mkdir mais_uma_pasta
-cp -r Maria mais_uma_pasta
-cp -r Joao mais_uma_pasta
+cp -r joao mais_uma_pasta/
+cp -r maria mais_uma_pasta/
 ### Nao apague a linha acima! ###
